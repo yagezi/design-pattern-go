f42f43ba7d42cbfc78a08a24fa68592fc138c504
Karl Lu <lkcnju@126.com>
2021-12-04 18:25:30 +0800

    Create command2.go

---
+++ command/command2.go
@@ -0,0 +1,35 @@
+/*
+*@Time        : 2021/07/03
+*@Creator     : lu.kaicheng 10261316
+*@File        : command2.go
+*@Description : 命令模式。函数式编程实现
+ */
+
+package command
+
+import "fmt"
+
+// Command 类型
+type TCommand func() error
+
+func StartCommandFunc(user_ string) TCommand {
+	start := &Start{}
+	return func() error {
+		start.Login(user_)
+		return start.Run()
+	}
+}
+
+func ArchiveCommandFunc() TCommand {
+	archive := &Archive{}
+	return func() error {
+		archive.Save()
+		return nil
+	}
+}
+
+func Invoke(fn TCommand) {
+	if err := fn(); err != nil {
+		fmt.Println(err.Error())
+	}
+}
