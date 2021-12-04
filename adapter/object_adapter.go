bd167ec4ae182d46bbf5c6103a1fac2e365c8ee4
Karl Lu <lkcnju@126.com>
2021-12-04 13:53:10 +0800

    Create object_adapter.go

---
+++ adapter/object_adapter.go
@@ -0,0 +1,43 @@
+/*
+*@Time        : 2021/06/25
+*@Creator     : lu.kaicheng 10261316
+*@File        : object_adapter.go
+*@Description : 适配器模式扩展：对象适配器，通过组合实现适配
+ */
+
+package adapter
+
+type IUser interface {
+	Create() error
+}
+
+type TecentCloud struct{}
+
+func (tc TecentCloud) Pay() string {
+	return "Give me money, you'll be happy"
+}
+
+type Azure struct{}
+
+func (az Azure) StartUp() string {
+	return "A problem has been detected and Windows has been shut down to prevent damage to your system"
+}
+
+// Adapter
+type UserAdapter struct {
+	tc TecentCloud
+	az Azure
+}
+
+func NewUserAdapter(tc_ TecentCloud, az_ Azure) *UserAdapter {
+	return &UserAdapter{
+		tc: tc_,
+		az: az_,
+	}
+}
+
+func (u UserAdapter) Create() error {
+	u.tc.Pay()
+	u.az.StartUp()
+	return nil
+}
